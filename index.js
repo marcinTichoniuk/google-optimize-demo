3332680074cf3ce73b504c586d21be43581e2128
Marcin Tichoniuk <marcin@sology.eu>
2023-02-09 15:04:41 +0100

    Fix js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ console.log('working');
 const experimentId = 'c3zgXYyrQSennxn4aLr7wQ';
 const variantId = Math.floor(Math.random() * 2);
 
-gtag('event', 'experiment_impression', {​
-  'experiment_id': experimentId,
-  'variant_id': experimentId.variantId,
-  'send_to': 'G-0C961JB7GE',
+gtag('event', 'experiment_impression', {
+  experiment_id: experimentId,
+  variant_id: experimentId.variantId,
+  send_to: 'G-0C961JB7GE',
 });
 
-const pElement = document.querySelector('p')
-if(variantId === 0){
-  pElement.textContent = 'original variant'
+const pElement = document.querySelector('p');
+if (variantId === 0) {
+  pElement.textContent = 'original variant';
 } else {
-  pElement.textContent = 'second variant'
+  pElement.textContent = 'second variant';
 }
