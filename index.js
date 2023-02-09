d0b18b433413cde70a05a9ac7ab32ba3a4c640d0
Marcin Tichoniuk <marcin@sology.eu>
2023-02-09 15:02:16 +0100

    Add server side test

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('working');
+
+const experimentId = 'c3zgXYyrQSennxn4aLr7wQ';
+const variantId = Math.floor(Math.random() * 2);
+
+gtag('event', 'experiment_impression', {​
+  'experiment_id': experimentId,
+  'variant_id': experimentId.variantId,
+  'send_to': 'G-0C961JB7GE',
+});
+
+const pElement = document.querySelector('p')
+if(variantId === 0){
+  pElement.textContent = 'original variant'
+} else {
+  pElement.textContent = 'second variant'
+}
